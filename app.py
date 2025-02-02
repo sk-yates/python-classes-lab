7b91f7ed54ff56750c2137ce5b6e28923f80a96b
sk-yates <syates.edu@gmail.com>
2025-02-02 17:21:47 +0000

    Final steps complete

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class Game():
             }
     def play_game(self):
         print(f"Welcome to the game, {self.turn} goes first!")
+        self.render()
     
     def print_board(self):
         b = self.board
@@ -36,28 +37,88 @@ class Game():
             move = input(f"Enter a valid move (example: A1): ").lower()
             
             if move in self.board and self.board[move] == None:
+                self.board[move] = self.turn
                 return move
             elif move in self.board and self.board[move] != None:
                 print("That space is taken, choose another")
             else: 
                 print("Invalid move! Please enter an available space on the board.")
     
+    def check_for_winner(self):
+        if self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1']):
+            self.winner = self.board['a1']
+            return True
+
+        elif self.board['a2'] and (self.board['a2'] == self.board['b2'] == self.board['c2']):
+            self.winner = self.board['a2']
+            return True
+            
+        elif self.board['a3'] and (self.board['a3'] == self.board['b3'] == self.board['c3']):
+            self.winner = self.board['a3']
+            return True
+            
+        elif self.board['a1'] and (self.board['a1'] == self.board['a2'] == self.board['a3']):
+            self.winner = self.board['a1']
+            return True
+            
+        elif self.board['b1'] and (self.board['b1'] == self.board['b2'] == self.board['b3']):
+            self.winner = self.board['b1']
+            return True
+            
+        elif self.board['c1'] and (self.board['c1'] == self.board['c2'] == self.board['c3']):
+            self.winner = self.board['c1']
+            return True
+            
+        elif self.board['a1'] and (self.board['a1'] == self.board['b2'] == self.board['c3']):
+            self.winner = self.board['a1']
+            return True
+            
+        elif self.board['c1'] and (self.board['c1'] == self.board['b2'] == self.board['a3']):
+            self.winner = self.board['c1']
+            return True
+        
+        return False
+
+
+    def check_for_tie(self):
+        board_full = True
+        for pos in self.board:
+            if self.board[pos] == "" or self.board[pos] == None:
+                board_full = False
+
+        winner_exists = self.check_for_winner()
+        
+        if board_full and not winner_exists:
+            self.tie = True
+        
+        else:
+            self.tie = False
+
+    def switch_turn(self):
+        turn_lookup = {'X': 'O', 'O': 'X'}
+        self.turn = turn_lookup[self.turn]
+
+    
     def render(self):
-        game_instance.print_board()
-        game_instance.print_message()
-        game_instance.get_move()
-    
-    
+        while not self.winner and not self.tie:
+            self.print_board()
+            self.print_message()
+            self.get_move()
+            self.check_for_winner()
+            self.check_for_tie()
+
+            if not self.winner and not self.tie:
+                self.switch_turn()
+        
+        self.print_board()
+        self.print_message()
     
     
     def __str__(self):
         return 'This is the Game() class.'
-
 
 # Instantiate the Game class:
 game_instance = Game()
 
 # Invoke the play_game method:
 game_instance.play_game()
-game_instance.render()
-
